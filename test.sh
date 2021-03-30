1c424c6c94b07c1e3d9bea0470698748a1862fd6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-03-30 18:09:24 +0200

    requirements

--- test.sh
+++ test.sh
@@ -1,11 +1,3 @@
-python3 -m pip install --upgrade pip jupyterlab==3.0.3
-
-which jupyterlab
-
-which python
-
-which python3
-
 yarn run start:detached
 
 ./wait-for-it.sh localhost:8888/api -- echo "JupyterLab is ready"
